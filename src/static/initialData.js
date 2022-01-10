3706356310397a0bb90359df2ae9819ac5737689
Paras Anand <speed1992@gmail.com>
2022-01-10 13:17:10 +0530

    created initial static data

---
+++ src/static/initialData.js
@@ -0,0 +1,16 @@
+let dataSet = {
+    tasks: {
+        "task-1": { title: "Technical Design", description: "Description for task 1" },
+        "task-2": { title: "Initialize Project", description: "Description for task-2" },
+        "task-3": { title: "Deliver Features", description: "Description for task-3" },
+        "task-4": { title: "Deployment", description: "Description for task-4" }
+    },
+    columns: {
+        "column-1": { id: "column-1", title: "To Do", taskIds: ['task-1'] },
+        "column-2": { id: "column-2", title: "In Progress", taskIds: ['task-2', 'task-3'] },
+        "column-3": { id: "column-3", title: "Done", taskIds: [] },
+    },
+    columnOrder: ["column-1", "column-2", "column-3"]
+}
+
+export default dataSet
